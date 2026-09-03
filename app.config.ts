9e9dcc4dad8b7e52b6e33e43dbb7fd1b9e881c90
lorishsudev <lorishsu.dev@gmail.com>
2026-09-03 23:46:05 +0800

    build: wire gradle memory plugin and raise eas resource class

--- app.config.ts
+++ app.config.ts
@@ -68,6 +68,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   // SDK 57 起原生 splash screen 改由 expo-splash-screen 這個 config plugin 負責,
   // 不再吃最上層的 splash 欄位
   plugins: [
+    "./plugins/withGradleMemory",
     "expo-sqlite",
     "@react-native-community/datetimepicker",
     "expo-font",
